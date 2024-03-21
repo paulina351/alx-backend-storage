8dcfd1c9c23c976eb2d95d31f0769adf95186027
Paulina Udeh <udehpaulina93@gmail.com>
2024-03-21 19:28:16 +0000

    we are all unique

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- A SQL Script that creates a table users
-USE `holberton`
+USE `holberton`;
 CREATE TABLE IF NOT EXIST users (
 	Id int NOT NULL | AUTO_INCREMENT | PRIMARY KEY,
 	Email varchar(255) NOT NULL |UNIQUE,
-	Name varchar(255),
+	Name varchar(255)
 );
