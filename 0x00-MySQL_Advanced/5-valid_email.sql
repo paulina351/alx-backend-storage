d0bf71079287d37368f2c0d4228f21b1bca03e5a
Paulina Udeh <udehpaulina93@gmail.com>
2024-03-22 04:51:54 +0000

    Email validation to sent

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,10 +1,9 @@
 --  a SQL script that creates a trigger that resets
 -- the attribute valid_email only when the email has been changed.
-DELIMITER
-$$
-USE `users`
+DELIMITER $$
+USE `holberton`$$
 CREATE
-[DEFINER = { `paulina`@`localhost` }]
+DEFINER =`paulina`@`localhost`
 TRIGGER `users`.`reset`
 AFTER UPDATE
 ON `users`.`valid_email` FOR EACH ROW
@@ -12,5 +11,4 @@ BEGIN
 	INSERT into users VALUES (user(), CONCAT('Update email ',
          OLD.email,' email :',OLD.valid_email,' valid_email ',
          NEW.valid_email));
-END;
-$$
+END$$
