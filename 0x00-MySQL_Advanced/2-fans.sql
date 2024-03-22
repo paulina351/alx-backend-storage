5ebf3de2832510b8055282236a8597d54d35aeb2
Paulina Udeh <udehpaulina93@gmail.com>
2024-03-22 04:05:21 +0000

    Best band ever

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,7 @@
 -- a SQL script that ranks country origins of bands,
 -- ordered by the number of (non-unique) fans
-USE holberton;
-CREATE TABLE country IF NOT EXISTS (
-	origin VARCHAR(255) NOT NULL,
-	nb_fans VARCHAR(255) NPT NULL,
+USE `holberton`;
+CREATE TABLE IF NOT EXISTS metal_bands (
+    origin VARCHAR(255) NOT NULL,
+    nb_fans VARCHAR(255) NOT NULL
 );
