1ea5437a8bc39d088565bb90f51c8f9aa849cb35
Paulina Udeh <udehpaulina93@gmail.com>
2024-03-22 04:12:12 +0000

    old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,9 +1,8 @@
 -- a SQL script that lists all bands with Glam rock
 -- as their main style, ranked by their longevity
 USE holberton;
-CREATE TABLE glam IF NOT EXISTS (
-        band_name VARCHAR(255) NOT NULL,
-        lifespan VARCHAR(255) NOT NULL,
+CREATE TABLE IF NOT EXISTS metal_bands (
+    band_name VARCHAR(255) NOT NULL,
+    lifespan VARCHAR(255) NOT NULL
 );
-
-INSERT INTO glam(lifespan) VALUES('formed', 'spilt');
+INSERT INTO metal_bands(lifespan) VALUES('formed', 'spilt');
