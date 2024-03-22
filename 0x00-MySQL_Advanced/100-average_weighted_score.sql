3db75b626ecc22cf3a66d94a24605d488984d473
Paulina Udeh <udehpaulina93@gmail.com>
2024-03-22 05:01:34 +0000

    Average weighted score

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,18 @@
+-- a SQL script that creates a stored procedure
+-- ComputeAverageWeightedScoreForUser that computes 
+-- and store the average weighted score for a student.
+CREATE [DEFINER = { user | CURRENT_USER }]
+PROCEDURE sp_name ([proc_parameter[,...]])
+[characteristic ...] routine_body
+proc_parameter: [ IN | OUT | INOUT ] param_name type
+type:
+Any valid MySQL data type
+characteristic:
+COMMENT 'string'
+| LANGUAGE SQL
+| [NOT] DETERMINISTIC
+| { CONTAINS SQL | NO SQL | READS SQL DATA
+| MODIFIES SQL DATA }
+| SQL SECURITY { DEFINER | INVOKER }
+routine_body:
+Valid SQL routine statement
