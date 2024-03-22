e93ddfcbe7aede0a28e60af7750ced3c02756f67
Paulina Udeh <udehpaulina93@gmail.com>
2024-03-22 04:46:03 +0000

    Buy buy buy

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,11 +1,13 @@
 -- A SQL script that creates a trigger that decreases the
 -- quantity of an item after adding a new order.
-DELIMITER
-$$
-USE `items`
+DELIMITER $$
+USE `holberton`$$
 CREATE     
-[DEFINER = { `paulina`@`localhost` }]
+DEFINER =`paulina`@`localhost`
 TRIGGER `items`.`fruits`
 AFTER DELETE
 ON `items`.`fruits` FOR EACH ROW
-$$
+BEGIN
+	INSERT into items VALUES (user(), CONCAT('After new order ',
+         OLD.items,' Clas :',OLD.quantity, '-> Deleted on ', NOW()));
+END$$
